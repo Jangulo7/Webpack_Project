04994d6e5a9364f097b8cb55889018569985f6da
Jangulo7 <johanna.angulo@gmail.com>
2021-07-05 02:17:36 -0400

    Webpack modes config

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,8 +3,8 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-    entry: './src/client/index.js',
     mode: 'production',
+    entry: './src/client/index.js',
     module: {
         rules: [
             {
